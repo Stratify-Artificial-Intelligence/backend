e9892c81ff768c05a1a500d2eec3a381e0847ece
pchs20 <pauduranmanzano@gmail.com>
2025-02-13 20:52:03 +0100

    [S-8] First endpoint for testing

---
+++ stratify_backend/main.py
@@ -0,0 +1,30 @@
+"""Implementations of entrypoint methods of Stratify Backend."""
+
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from stratify_backend.routers import tests
+
+app = FastAPI(
+    title='Stratify Backend API',
+    description='Backend module of Stratify Artificial Intelligence.',
+    root_path=os.getenv('ROOT_PATH', ''),
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+app.include_router(tests.router)
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', log_level='debug', port=8080, reload=True)
