e3ad77e3cdf7b833e5611ad22e95abe14afc9479
pchs20 <pauduranmanzano@gmail.com>
2025-02-26 14:10:18 +0100

    [S-25] Integration tests WIP and commented

--- stratify_backend/app/tests/conftest.py
+++ stratify_backend/app/tests/conftest.py
@@ -3,16 +3,42 @@ from typing import AsyncGenerator
 import pytest
 import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.postgresql import get_session
 from app.main import app
 
 
-@pytest_asyncio.fixture
-async def db_session() -> AsyncGenerator[AsyncSession, None]:
-    async for session in get_session():
-        yield session
+# @pytest.fixture(scope='session')
+# def event_loop(request) -> Generator:
+#     """Create an instance of the default event loop for each test case."""
+#     # ToDo (pduran): This seems to not work as expected
+#     loop = asyncio.get_event_loop_policy().new_event_loop()
+#     yield loop
+#     loop.close()
+
+
+# @pytest_asyncio.fixture(scope="session", autouse=True)
+# async def setup_test_db():
+#     """Create and drop tables before/after tests."""
+#     async with engine.begin() as connection:
+#         await connection.run_sync(Base.metadata.create_all)
+#         yield
+#         await connection.run_sync(Base.metadata.drop_all)
+
+
+# @pytest_asyncio.fixture
+# async def db_session() -> AsyncGenerator[AsyncSession, None]:
+#     async for session in get_session():
+#         yield session
+#         await session.rollback()
+
+
+# @pytest.fixture(scope="function")
+# def override_get_db(db_session: AsyncSession) -> Callable:
+#     """Override FastAPI's get_db dependency."""
+#     async def _override_get_db():
+#         yield db_session
+#
+#     return _override_get_db
 
 
 @pytest_asyncio.fixture()
