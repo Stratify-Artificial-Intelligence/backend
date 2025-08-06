b1036d9fc4915a046ff495abce497954ba725f03
Pau Duran <79756450+pchs20@users.noreply.github.com>
2025-08-06 16:37:06 +0200

    [S-250] Stripe automatic tax enabled (#79)

--- backend/app/services/stripe.py
+++ backend/app/services/stripe.py
@@ -35,6 +35,8 @@ async def create_checkout_session(
         success_url=success_url,
         cancel_url=cancel_url,
         allow_promotion_codes=True,
+        automatic_tax={"enabled": True},
+        customer_update={"address": "auto"},
     )
 
 
@@ -68,5 +70,12 @@ async def create_customer(email: str, name: str | None = None) -> stripe.Custome
     return stripe.Customer.create(
         email=email,
         name=name if name is not None else email,
-        description='Created from the Backend application',
+        description='Created from the Backend application. Address hardcoded.',
+        # address={
+        #     'line1': 'Carrer de Pau Claris, 138',
+        #     'city': 'Barcelona',
+        #     'postal_code': '08009',
+        #     'state': 'Barcelona',
+        #     'country': 'ES',
+        # },
     )
