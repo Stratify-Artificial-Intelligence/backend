1cad07a255f2fef8576d1212cd2b353c5b0b5fb7
Pau Duran <79756450+pchs20@users.noreply.github.com>
2025-08-15 12:54:21 +0200

    [S-276] POST /researches allowed for basic users (#82)

--- backend/app/routers/business_research.py
+++ backend/app/routers/business_research.py
@@ -113,7 +113,9 @@ async def store_research_by_id(  # noqa: C901
         status.HTTP_403_FORBIDDEN: {'model': schemas.HTTP403Forbidden},
         status.HTTP_404_NOT_FOUND: {'model': schemas.HTTP404NotFound},
     },
-    dependencies=[Depends(RoleChecker(allowed_roles=[UserRoleEnum.ADMIN]))],
+    dependencies=[
+        Depends(RoleChecker(allowed_roles=[UserRoleEnum.ADMIN, UserRoleEnum.BASIC])),
+    ],
 )
 async def create_research(
     research_params: ResearchParams,
